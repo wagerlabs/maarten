81bc767516a824e7f83ab512c02e73913a845fd6
wagerlabs <69521622+wagerlabs@users.noreply.github.com>
2020-08-11 17:50:25 +0200

    Add files via upload

---
+++ js/liveperson-seo-en.js
@@ -0,0 +1,8 @@
+window.setTimeout(function(){
+LR_showInviteDiv("مرحبا يا صديقي", '<div id="myinfo"><span id="my_want_more">إذا كنت تريد أن تعرف المزيد عن منتجاتنا ولنا، اضغط هنا والحصول على مزيد من المعلومات.</div>');
+},3000);
+
+document.writeln("<div style=\"width:120px;height:245px;float:left; z-index:999;position:fixed !important; top/**/:210px; position:absolute; top:expression(offsetParent.scrollTop+400);left:10px;\"> ");
+document.write('<script type="text/javascript" src="http://pkt.zoosnet.net/JS/LsJS.aspx?siteid=PKT21646611&float=0&lng=en"></script>');
+document.writeln("<a href=\"javascript:void(0)\" onclick=openZoosUrl(\'chatwin\')  target=\"_self\"><img src=\"/img/reponline-1.gif\" border=\"0\"></a>");
+document.writeln("</div>");
